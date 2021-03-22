943f6bec33b73c58b230678661d341a4372d78e4
dgmar <dgmar@DESKTOP-OVV763L>
2021-03-22 08:54:27 +0100

    MarioRama

--- src/repaso_carlos/mario/Calcular.java
+++ src/repaso_carlos/mario/Calcular.java
@@ -1,5 +1,7 @@
 package repaso_carlos.mario;
 
+import java.util.Scanner;
+
 public class Calcular {
 
 	public static void main(String[] args) {
@@ -9,7 +11,14 @@ public class Calcular {
 	}
 
 	public static void sumar() {
+		Scanner ent=new Scanner(System.in);
+		int num1=0 , num2=0;
 		
-		
+		System.out.println("Introduce el primer numero");
+		num1=ent.nextInt();
+		System.out.println("Introduce el segundo numero");
+		num2=ent.nextInt();
+		int total= num1+num2;
+		System.out.println(num1 +"+"+num2+"="+total);
 	}
 }
